47e863d3bffc07e1de2c18acb49fc7d7b61ab51c
AJ <prabhathxdissanayake@gmail.com>
2024-05-13 17:05:07 +0530

    app.js sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+
+    <section className="padding">
+      PopularProduct
+    </section>
+
+    <section className="padding">
+      SuperQuality
+    </section>
+
+    <section className="padding">
+      Popular Product
+    </section>
+
+    <section className="padding">
+      Popular Product
+    </section>
+
+  </main>
+);
+
+export default App;
